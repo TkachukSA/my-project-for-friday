ef84171dba2ab6880b59fdd676ba9c4f1e013ec2
Berenas54 <haravoy1998@mail.ru>
2021-03-07 22:40:49 +0300

    Накидал инпуты. Комит для теста гита.

--- src/auth/registration/Registration.tsx
+++ src/auth/registration/Registration.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import s from "./Registration.module.css"
+
 
 export const Registration = () => {
+    return (<div className={s.wrapper}>
+        <input type={"text"} placeholder={"Email"}/>
+        <input type={"text"} placeholder={"Password"}/>
+        <button className={s.btn}>Register</button>
+    </div>)
 
-    return<>
-        <div> Registration page</div>
-    </>
-    
 }
