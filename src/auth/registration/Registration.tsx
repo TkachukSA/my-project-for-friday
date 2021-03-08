9a99305bd0b38c4971260283501f7d579cfb59ea
Berenas54 <haravoy1998@mail.ru>
2021-03-08 20:37:04 +0300

    сделал стейт для страницы

--- src/auth/registration/Registration.tsx
+++ src/auth/registration/Registration.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import s from "./Registration.module.css"
 
+type RegistrationStateType = {
+    newEmail: string,
+    newPassword: string,
+    confirmedPassword: string
+}
+
+
+const RegistrationState: RegistrationStateType = {
+    newEmail: "Haravoy1998@mail.ru",
+    newPassword: "123456789",
+    confirmedPassword: "123456789"
+}
 
 export const Registration = () => {
     return (<div className={s.wrapper}>
-        <input type={"text"} placeholder={"Email"}/>
-        <input type={"text"} placeholder={"Password"}/>
+        <input className={s.input} type={"text"} placeholder={"Email"} value={RegistrationState.newEmail}/>
+        <input className={s.input} type={"password"} placeholder={"Password"} value={RegistrationState.newPassword}/>
+        <input className={s.input} type={"password"} placeholder={" Confirm password"} value={RegistrationState.confirmedPassword}/>
         <button className={s.btn}>Register</button>
     </div>)
-
 }
