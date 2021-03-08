d1cfb5286626c6eab7f402581b5eeeff62e7132c
Berenas54 <haravoy1998@mail.ru>
2021-03-08 23:54:01 +0300

    Registration page version 1

--- src/bll/reducers/registration-reducer.ts
+++ src/bll/reducers/registration-reducer.ts
@@ -1,38 +1,58 @@
 import {Dispatch} from 'redux'
+import {authAPI, dataNewUserType} from "../../api/api";
 
 
-type RegistrationStateType = {
-    newEmail: string,
-    newPassword: string,
-    confirmedPassword: string
+export type RegistrationStateType = {
+    error: string,
+    redirect: boolean
 }
 
 const initialState: RegistrationStateType = {
-    newEmail: "",
-    newPassword: "",
-    confirmedPassword: ""
+    error: "",
+    redirect: false
 }
 
 type InitialStateType = typeof initialState
 
-export const authReducer = (state: InitialStateType = initialState, action: ActionsType): RegistrationStateType => {
+export const registrationReducer = (state: InitialStateType = initialState, action: ActionsType): RegistrationStateType => {
     switch (action.type) {
-        case 'login/SET-IS-LOGGED-IN':
-            return state
+        case 'registration/ERROR':
+            return {
+                ...state, error: action.error
+            }
+        case 'registration/SET-REDIRECT':
+            return {
+                ...state, redirect: action.redirect
+            }
         default:
             return state
     }
 }
 
 // actions
-export const setIsLoggedInAC = (value: boolean) =>
-    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
+export const setErrorAC = (error: string) =>
+    ({type: 'registration/ERROR', error} as const)
+export const setRedirectAC = (redirect: boolean) =>
+    ({type: 'registration/SET-REDIRECT', redirect} as const)
+
 
 // thunks
-export const loginTC = (data: any) => (dispatch: Dispatch<ActionsType>) => {
+export const registrationTC = (data: dataNewUserType) => (dispatch: Dispatch<ActionsType>) => {
 
+    authAPI.registration(data)
+        .then((res) => {
+            if (!res.data.error) {
+                console.log("registration done")
+                dispatch(setRedirectAC(true))
+            } else {
+                console.log(res.data.error)
+            }
+        })
+        .catch((error) => {
+            dispatch(setErrorAC(error))
+        })
 }
 
 
 // types
-type ActionsType = ReturnType<typeof setIsLoggedInAC>
+type ActionsType = ReturnType<typeof setErrorAC | typeof setRedirectAC>
--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -1,14 +1,17 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {profileReducer} from "../reducers/profile-reducer";
 import {authReducer} from "../reducers/auth-reducer";
+import {registrationReducer} from "../reducers/registration-reducer";
+import thunkMiddleware from 'redux-thunk'
 
 
 const reducers = combineReducers({
     profile: profileReducer,
-    auth: authReducer
+    auth: authReducer,
+    registration: registrationReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 export default store
 
 export type AppStoreType = ReturnType<typeof reducers>
